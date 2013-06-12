07c5bcb9aa7b479fa3909f0b23d309d207674a0c
Dan Young <dmy@pdx.edu>
2013-06-12 16:13:15 -0700

    Use more verbose variable for LINE

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for L in {1..100}
+for LINE in {1..100}
 do
-	FIZZ=$(($L % 3))
-	BUZZ=$(($L % 5))
+	FIZZ=$(($LINE % 3))
+	BUZZ=$(($LINE % 5))
 
 	[ $FIZZ -eq 0 ] && echo -n Fizz
 	[ $BUZZ -eq 0 ] && echo -n Buzz
-	[ $FIZZ -ne 0 ] && [ $BUZZ -ne 0 ] && echo -n $L
+	[ $FIZZ -ne 0 ] && [ $BUZZ -ne 0 ] && echo -n $LINE
 
 	echo
 done
