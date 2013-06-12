0b21ec13dbee53952c46ec983b9ed5329e531db7
Dan Young <dmy@pdx.edu>
2013-06-12 16:01:53 -0700

    remove trailing whitespace

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -4,7 +4,7 @@ for L in {1..100}
 do
 	FIZZ=$(($L % 3))
 	BUZZ=$(($L % 5))
-	
+
 	[ $FIZZ -eq 0 ] && echo -n Fizz
 	[ $BUZZ -eq 0 ] && echo -n Buzz
 	[ $FIZZ -ne 0 ] && [ $BUZZ -ne 0 ] && echo -n $L
