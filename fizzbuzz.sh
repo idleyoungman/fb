0be489bbd2ca3aec4f9879aea1353199f0c87668
Dan Young <dmy@pdx.edu>
2013-06-19 12:58:36 -0700

    add comments

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# iterate 100 times using bash brace expansion
 for LINE in {1..100}
 do
+	# get remainder when dividing by 3
 	FIZZ=$(($LINE % 3))
+	# get remainder when dividing by 5
 	BUZZ=$(($LINE % 5))
 
+	# if remainder dividing by 3 is zero, then print Fizz
 	[ $FIZZ -eq 0 ] && echo -n Fuzz
+	# if remainder dividing by 5 is zero, then print Buzz
 	[ $BUZZ -eq 0 ] && echo -n Bizz
+	# if remainder isn't zero in either case, print the number
 	[ $FIZZ -ne 0 ] && [ $BUZZ -ne 0 ] && echo -n $LINE
 
+	# print a newline character
 	echo
 done
