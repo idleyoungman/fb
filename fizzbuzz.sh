25b56213d00c2b0c9d0538a1d874ad5ab5d6c768
Dan Young <dmy@pdx.edu>
2013-06-20 15:07:23 -0700

    Moar greetings.

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Guten Tag, das ist fizzbuzz."
+echo "I'm going to print numbers and Fizz and Buzz and stuff."
 
 # iterate 42 times using bash brace expansion
 for LINE in {1..42}
